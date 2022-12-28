fix: root finding algorithm sign bug

lm/model.py
@@ -76,10 +76,11 @@ class Model(pl.LightningModule):
         
     def forward(self, a: "B,A,L", mask: "B,N", tokens: "B,N", times: "B,N"):
         z: "B,L,D" = self.enc(a.permute(0,2,1))
+        context_mask = torch.ones(z.shape[:-1], dtype=bool, device=z.device)
         out: "B,N,V+T" = self.dec(torch.cat([
             self.token_embeddings(tokens),
             self.to_time_embedding(times),
-        ], dim=2), context=z, mask=mask.bool(), context_mask = torch.ones(z.shape[:-1], dtype=bool))
+        ], dim=2), context=z, mask=mask.bool(), context_mask = context_mask)
         return torch.tensor_split(out, (VOCAB_SIZE,), dim=-1)
     
     

osu_dreamer/signal/fit_bezier.py
@@ -140,4 +140,4 @@ def newton_raphson_root_find(bez: "4,2", points: "L,2", u: "L,"):
     num = (d * qp).sum(-1)
     den = (qp**2 + d*qprimeprime(bez, u)).sum(-1)
     
-    return u + np.where(den==0, 0, num/den)
+    return u - np.divide(num, den, out=np.zeros_like(num), where=den!=0)