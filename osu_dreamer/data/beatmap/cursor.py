90931a106e5bf7b0f20b770d0d7837147eefef7e
jason <jason@jwon.xyz>
2024-05-01 18:13:45 +0000

    feat: encode doesn't move cursor before object appears

--- osu_dreamer/data/beatmap/cursor.py
+++ osu_dreamer/data/beatmap/cursor.py
@@ -8,9 +8,14 @@ import numpy as np
 from numpy import ndarray
 
 from osu_dreamer.osu.beatmap import Beatmap
-from osu_dreamer.osu.hit_objects import  Slider, Spinner, HitObject
+from osu_dreamer.osu.hit_objects import Circle, Slider, Spinner, HitObject
 
 from ..load_audio import FrameTimes
+
+def preempt(ar: float) -> float:
+    """compute duration a hit object stays on screen from Approach Rate (AR)"""
+    factor = 120 if ar <= 5 else 150
+    return 1200 + factor * (5-ar)
 
 def cursor_signal(bm: Beatmap, frame_times: FrameTimes) -> Float[ndarray, "2 L"]:
     """
@@ -21,12 +26,14 @@ def cursor_signal(bm: Beatmap, frame_times: FrameTimes) -> Float[ndarray, "2 L"]
     (0,0) -- (1,0)
     """
     hos = iter(bm.hit_objects)
-    cur: Optional[HitObject] = None
+    cur: HitObject = Circle(0, False, 256, 192)
     nxt: Optional[HitObject] = next(hos, None)
     if nxt is None:
         warnings.warn('beatmap has no hit objects')
 
     pos: list[Float[ndarray, "2"]] = []
+
+    bm_preempt = preempt(bm.ar)
 
     for t in frame_times:
 
@@ -34,14 +41,7 @@ def cursor_signal(bm: Beatmap, frame_times: FrameTimes) -> Float[ndarray, "2 L"]
         while nxt is not None and nxt.t <= t:
             cur, nxt = nxt, next(hos, None)
 
-        if cur is None:
-            # before first hit object
-            if nxt is None:
-                # beatmap has no hit objects
-                pos.append(np.array([256,192]))
-            else:
-                pos.append(nxt.start_pos())
-        elif t < cur.end_time():
+        if t < cur.end_time():
             # hitting current hit object
             # note: will not be a `Circle` by definition of `cur`
             if isinstance(cur, Spinner):
@@ -56,8 +56,14 @@ def cursor_signal(bm: Beatmap, frame_times: FrameTimes) -> Float[ndarray, "2 L"]
             # after last hit object
             pos.append(cur.end_pos())
         else:
-            # moving (lerp) to next hit object
-            f = (t - cur.end_time()) / (nxt.t - cur.end_time())
-            pos.append((1 - f) * cur.end_pos() + f * nxt.start_pos())
+            # current object completed, look for next object
+            if nxt.t - t > bm_preempt:
+                # next object hasn't appeared yet, don't approach
+                pos.append(cur.end_pos())
+            else:
+                # move (lerp) to next hit object
+                start_time = max(cur.end_time(), nxt.t - bm_preempt)
+                f = (t - start_time) / (nxt.t - start_time)
+                pos.append((1 - f) * cur.end_pos() + f * nxt.start_pos())
         
     return (np.array(pos) / np.array([512,384])).T
