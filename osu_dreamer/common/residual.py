f41f1fa00f43e2445d790f4c7be608dde8436f1b
jason <jason@jwon.xyz>
2024-08-20 15:25:47 +0000

    feat: no resstack norm

--- osu_dreamer/common/residual.py
+++ osu_dreamer/common/residual.py
@@ -6,8 +6,6 @@ import torch as th
 from torch import nn, Tensor
 import torch.nn.functional as F
 
-from osu_dreamer.common.norm import RMSNorm
-
 class Residual(nn.Module):
     def __init__(self, net: nn.Module):
         super().__init__()
@@ -24,7 +22,6 @@ class ResStack(nn.Module):
     ):
         super().__init__()
         self.blocks = nn.ModuleList(blocks)
-        self.norms = nn.ModuleList([ RMSNorm(dim) for _ in blocks ])
         self.outs = nn.ModuleList([ nn.Conv1d(dim, 2*dim, 1) for _ in blocks ])
 
     def forward(
@@ -34,10 +31,10 @@ class ResStack(nn.Module):
     ) -> Float[Tensor, "B D L"]:
         
         o = th.zeros_like(x)
-        for block, norm, out in zip(self.blocks, self.norms, self.outs):
+        for block, out in zip(self.blocks, self.outs):
             h = block(x, *args, **kwargs)
             res, skip = out(h).chunk(2, dim=1)
-            x = F.silu(norm(x + res))
+            x = F.silu(x + res)
             o = o + skip
 
         return o
