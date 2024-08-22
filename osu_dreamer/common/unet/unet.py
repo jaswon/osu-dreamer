623f632ae99460d238604938ba0ee55c84ba39c7
jason <jason@jwon.xyz>
2024-08-22 16:20:02 +0000

    unet: refactor split

--- osu_dreamer/common/unet/unet.py
+++ osu_dreamer/common/unet/unet.py
@@ -3,23 +3,9 @@ from collections.abc import Callable
 from jaxtyping import Float
 
 from torch import nn, Tensor
-
-from osu_dreamer.common.norm import RMSNorm
+import torch.nn.functional as F
 
 from .pad import pad, unpad
-
-class Join(nn.Module):
-    def __init__(self, dim: int):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv1d(dim, dim, 5,1,2, groups=dim),
-            nn.Conv1d(dim, dim, 1),
-            RMSNorm(dim),
-            nn.SiLU(),
-        )
-
-    def forward(self, h: Float[Tensor, "B X L"], x: Float[Tensor, "B X L"]) -> Float[Tensor, "B X L"]:
-        return self.net(h) * x
 
 class UNet(nn.Module):
     def __init__(
@@ -32,18 +18,18 @@ class UNet(nn.Module):
         super().__init__()
 
         self.pre = nn.ModuleList()
+        self.split = nn.ModuleList()
         self.down = nn.ModuleList()
-        self.post = nn.ModuleList()
-        self.join = nn.ModuleList()
         self.up = nn.ModuleList()
+        self.post = nn.ModuleList()
 
         for scale in scales:
             self.pre.append(block())
-            self.down.append(nn.Conv1d(dim, dim, scale+2,scale,1, groups=dim))
+            self.split.append(nn.Conv1d(dim, 2*dim, 1))
+            self.down.append(nn.Conv1d(dim, dim, scale, scale, groups=dim))
             
             self.post.insert(0, block())
-            self.join.insert(0, Join(dim))
-            self.up.insert(0, nn.ConvTranspose1d(dim, dim, scale+2,scale,1, groups=dim))
+            self.up.insert(0, nn.ConvTranspose1d(dim, dim, scale, scale, groups=dim))
 
         self.middle = middle
 
@@ -60,16 +46,17 @@ class UNet(nn.Module):
         x, p = pad(x, self.chunk_size)
 
         hs = []
-        for pre, down in zip(self.pre, self.down):
-            x = pre(x)
-            hs.append(x)
+        for pre, split, down in zip(self.pre, self.split, self.down):
+            x = pre(x, *args, **kwargs)
+            h, x = split(x).chunk(2, dim=1)
+            hs.append(h)
             x = down(x)
             
         x = self.middle(x, *args, **kwargs)
 
-        for up, join, post in zip(self.up, self.join, self.post):
+        for up, post in zip(self.up, self.post):
             x = up(x)
-            x = join(hs.pop(), x)
-            x = post(x)
+            x = F.silu(hs.pop()) * x
+            x = post(x, *args, **kwargs)
 
         return unpad(x, p)
