67ac54bc53e5bf96460e83dc5baf700824629ea3
jason <jason@jwon.xyz>
2024-08-22 16:20:16 +0000

    feat: res stack uses norm

--- osu_dreamer/common/residual.py
+++ osu_dreamer/common/residual.py
@@ -2,9 +2,10 @@
 from collections.abc import Sequence
 from jaxtyping import Float
 
-import torch as th
 from torch import nn, Tensor
 import torch.nn.functional as F
+
+from osu_dreamer.common.norm import RMSNorm
 
 class Residual(nn.Module):
     def __init__(self, net: nn.Module):
@@ -23,6 +24,7 @@ class ResStack(nn.Module):
         super().__init__()
         self.blocks = nn.ModuleList(blocks)
         self.outs = nn.ModuleList([ nn.Conv1d(dim, 2*dim, 1) for _ in blocks ])
+        self.norms = nn.ModuleList([ RMSNorm(dim) for _ in blocks ])
 
     def forward(
         self,
@@ -31,10 +33,10 @@ class ResStack(nn.Module):
     ) -> Float[Tensor, "B D L"]:
         
         o = x
-        for block, out in zip(self.blocks, self.outs):
+        for block, out, norm in zip(self.blocks, self.outs, self.norms):
             h = block(x, *args, **kwargs)
             res, skip = out(h).chunk(2, dim=1)
-            x = F.silu(x + res)
+            x = F.silu(norm(x + res))
             o = o + skip
 
         return o
