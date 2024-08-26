d7bae5a46cac08b1a4e3a840df8b24253a56f0b3
jason <jason@jwon.xyz>
2024-08-26 01:22:25 +0000

    feat: critic no attention, wave net

--- osu_dreamer/model/modules/critic.py
+++ osu_dreamer/model/modules/critic.py
@@ -7,18 +7,15 @@ import torch as th
 from torch import nn, Tensor
 
 from osu_dreamer.common.residual import ResStack
-from osu_dreamer.common.linear_attn import RoPE, LinearAttn, AttnArgs
-
 
 
 @dataclass
 class CriticArgs:
     h_dim: int
-
+    a_pre_num_blocks: int
+    a_pre_block_depth: int
     scales: list[int]
     block_depth: int
-    stack_depth: int
-    attn_args: AttnArgs
 
 class Critic(nn.Module):
     def __init__(
@@ -29,26 +26,34 @@ class Critic(nn.Module):
     ):
         super().__init__()
 
-        self.rope = RoPE(args.attn_args.head_dim)
+        self.a_pre = nn.Sequential(
+            nn.Conv1d(a_dim, args.h_dim, 1),
+            ResStack(args.h_dim, [
+                nn.Sequential(
+                    nn.ZeroPad1d((1,0) if d==0 else 2**(d-1)),
+                    nn.Conv1d(
+                        args.h_dim, args.h_dim, 2, 
+                        dilation=2**d,
+                        groups=args.h_dim,
+                    )
+                )
+                for _ in range(args.a_pre_num_blocks)
+                for d in range(args.a_pre_block_depth)
+            ]) # wave net: context length = num_blocks*2^block_depth
+        )
+
         self.net = nn.Sequential(
-            nn.Conv1d(x_dim+a_dim, args.h_dim, 1), 
+            nn.Conv1d(x_dim+args.h_dim, args.h_dim, 1), 
             *(
                 block for scale in args.scales
                 for block in [
                     ResStack(args.h_dim, [
-                        nn.Conv1d(args.h_dim, args.h_dim, 5,1,2, groups=args.h_dim)
+                        nn.Conv1d(args.h_dim, args.h_dim, 1)
                         for _ in range(args.block_depth)
                     ]),
                     nn.Conv1d(args.h_dim, args.h_dim, scale, scale),
                 ]
             ),
-            ResStack(args.h_dim, [
-                block for _ in range(args.stack_depth)
-                for block in [
-                    LinearAttn(args.h_dim, self.rope, args.attn_args),
-                    nn.Conv1d(args.h_dim, args.h_dim, 5,1,2, groups=args.h_dim),
-                ]
-            ]),
             nn.Conv1d(args.h_dim, 1, 1),
         )
 
@@ -57,6 +62,5 @@ class Critic(nn.Module):
         a: Float[Tensor, "B A L"],
         p: Int[Tensor, "B L"],
         x: Float[Tensor, "B X L"],
-    ) -> Float[Tensor, "B"]:
-        o = self.net(th.cat([a,x], dim=1))
-        return o.squeeze(1).mean(dim=-1)
+    ) -> Float[Tensor, "B l"]:
+        return self.net(th.cat([self.a_pre(a),x], dim=1)).squeeze(1)
