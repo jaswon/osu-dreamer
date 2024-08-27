a1cbddfdc2bf63dd8529829e5ba0607e76d312d4
jason <jason@jwon.xyz>
2024-08-27 21:08:52 +0000

    tidy: one file unet

--- osu_dreamer/common/unet.py
+++ osu_dreamer/common/unet.py
@@ -3,8 +3,18 @@ from collections.abc import Callable
 from jaxtyping import Float
 
 from torch import nn, Tensor
+import torch.nn.functional as F
 
-from .pad import pad, unpad
+def pad(x: Float[Tensor, "... L"], size: int) -> tuple[Float[Tensor, "... Lp"], int]:
+    padding = (size-x.size(-1)%size)%size
+    if padding > 0:
+        x = F.pad(x, (0, padding))
+    return x, padding
+
+def unpad(x: Float[Tensor, "... Lp"], padding: int) -> Float[Tensor, "... L"]:
+    if padding > 0:
+        x = x[...,:-padding]
+    return x
 
 class UNet(nn.Module):
     def __init__(
