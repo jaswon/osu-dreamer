7f3a8eef389a9c66eafb544eb65cef9e6a1295ec
jason <jason@jwon.xyz>
2024-08-28 04:27:15 +0000

    revert: skip gen step

--- osu_dreamer/model/model.py
+++ osu_dreamer/model/model.py
@@ -34,7 +34,6 @@ class Model(pl.LightningModule):
         gen_lr: float,
         r1_gamma: float,
         gen_adv_factor: float,
-        skip_gen_step_threshold: float,
         critic_steps: int = 1,
         gen_steps: int = 1,
     ):
@@ -51,7 +50,6 @@ class Model(pl.LightningModule):
         self.gen_lr = gen_lr
         self.gen_adv_factor = gen_adv_factor
         self.r1_gamma = r1_gamma
-        self.skip_gen_step_threshold = skip_gen_step_threshold
         self.gen_steps = gen_steps
         self.critic_steps = critic_steps
     
@@ -130,9 +128,6 @@ class Model(pl.LightningModule):
 
         self.log('train/critic/adv', critic_adv_loss.detach())
         self.log('train/critic/r1', r1_loss.detach())
-
-        if critic_adv_loss > self.skip_gen_step_threshold > 0:
-            return
 
         #################### Train Generator ####################
 
