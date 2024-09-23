95a31ef11c41225c315d6290d9bbb0482639aca6
jason <jason@jwon.xyz>
2024-09-23 13:32:59 +0000

    rff: conv1d version

--- osu_dreamer/modules/rff.py
+++ osu_dreamer/modules/rff.py
@@ -1,9 +1,9 @@
 
-from jaxtyping import Float
-
 import torch as th
-from torch import nn, Tensor
+from torch import nn
 import torch.nn.functional as F
+
+from einops import rearrange
 
 class RandomFourierFeatures(nn.Module):
     def __init__(
@@ -11,12 +11,18 @@ class RandomFourierFeatures(nn.Module):
         dim: int, 
         n_feats: int,
         scale: float = 10.,
+        conv1x1: bool = False,
     ):
         super().__init__()
+        self.conv1x1 = conv1x1
+
         d = n_feats//2
         assert d * 2 == n_feats, '`n_feats` must be even'
         self.register_buffer('W', th.randn(dim, d) * scale)
 
-    def forward(self, x: Float[Tensor, "... D"]) -> Float[Tensor, "... E"]:
-        theta = (x * 2 * th.pi) @ self.W
-        return th.cat([theta.sin(), theta.cos()], dim=-1)
+    def forward(self, x):
+        if self.conv1x1:
+            theta = F.conv1d(x * 2 * th.pi, rearrange(self.W, 'i o -> o i 1'))
+        else:
+            theta = (x * 2 * th.pi) @ self.W
+        return th.cat([theta.sin(), theta.cos()], dim=1 if self.conv1x1 else -1)
