8d457eb9168fdf9d7e480a7329800dd659901add
jason <jason@jwon.xyz>
2024-10-22 19:22:52 +0000

    mod conv: refactor

--- osu_dreamer/modules/modconv.py
+++ osu_dreamer/modules/modconv.py
@@ -1,20 +1,27 @@
 
+from typing import Callable
 from jaxtyping import Float
 
 import torch as th
 from torch import nn, Tensor
+import torch.nn.functional as F
 
 from einops import rearrange
 
 class _ModConv(nn.Module):
+    conv: Callable
+
     def __init__(
         self,
         in_dim: int,
+        out_dim: int,
         t_dim: int,
-        conv: nn.modules.conv._ConvNd,
     ):
         super().__init__()
-        self.conv = conv
+
+        self.weight = nn.Parameter(th.empty(out_dim, in_dim))
+        th.nn.init.kaiming_uniform_(self.weight, a=5**.5)
+
         self.mod = nn.Sequential(
             nn.Linear(t_dim, t_dim),
             nn.SiLU(),
@@ -30,20 +37,18 @@ class _ModConv(nn.Module):
         bx,t = xt
         b = t.size(0)
 
-        bw = th.einsum('oi...,bi->boi...', self.conv.weight, self.mod(t)+1)
-        demod = th.rsqrt(th.sum(bw.flatten(2) ** 2, dim=-1) + 1e-8)
-        bw = th.einsum('boi...,bo->boi...', bw, demod)
+        bw = th.einsum('oi,bi->boi', self.weight, self.mod(t)+1)
+        demod = th.rsqrt(th.sum(bw ** 2, dim=2) + 1e-8)
+        bw = th.einsum('boi,bo->boi', bw, demod)
 
-        w = rearrange(bw, 'b o i ... -> (b o) i ...')
-        x = rearrange(bx, 'b d ... -> 1 (b d) ...')
-        self.conv.groups = b
-        o = self.conv._conv_forward(x, w, None)
+        w = rearrange(bw, 'b o i -> (b o) i')
+        x = rearrange(bx, 'b d ... -> 1 (b d) ...') 
+        o = type(self).conv(x, w, b)
+
         return rearrange(o, '1 (b d) ... -> b d ...', b=b)
 
 class ModulatedConv1d(_ModConv):
-    def __init__(self, in_dim: int, out_dim: int, t_dim: int):
-        super().__init__(in_dim, t_dim, nn.Conv1d(in_dim, out_dim, 1, bias=False))
+    conv = lambda x,w,b: F.conv1d(x, w[:,:,None], None, groups=b)
 
 class ModulatedConv2d(_ModConv):
-    def __init__(self, in_dim: int, out_dim: int, t_dim: int):
-        super().__init__(in_dim, t_dim, nn.Conv2d(in_dim, out_dim, 1, bias=False))
+    conv = lambda x,w,b: F.conv2d(x, w[:,:,None,None], None, groups=b)
