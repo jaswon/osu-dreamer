3f6943e627f0b58db511b00a81f5bca31a57d520
jason <jason@jwon.xyz>
2024-10-22 23:54:57 +0000

    minGRU: caller owns hg

--- osu_dreamer/modules/mingru.py
+++ osu_dreamer/modules/mingru.py
@@ -1,58 +1,24 @@
 
-from typing import Optional
 from jaxtyping import Float
 
 import torch as th
 from torch import nn, Tensor
 import torch.nn.functional as F
 
-def g(x):
-    return th.where(x >= 0, x + 1, th.exp(x))
+log_g = lambda x: th.where(x >= 0, th.log(F.relu(x)+1), x)
 
-def log_g(x):
-    return th.where(x >= 0, th.log(F.relu(x)+1), x)
+def min_gru(hg: Float[Tensor, "B H L"]) -> Float[Tensor, "B H/2 L"]:
+    h, gate = hg.chunk(2, dim=1)
 
-class minGRU(nn.Module):
-    """https://arxiv.org/pdf/2410.01201"""
+    log_coeffs = -F.softplus(gate)
+    log_values = -F.softplus(-gate) + log_g(h)
 
-    def __init__(self, dim: int, h_dim: int):
-        super().__init__()
-        self.hg = nn.Conv1d(dim, 2*h_dim, 1)
-
-    def step(
-        self,
-        x: Float[Tensor, "B D 1"],
-        prev_hidden: Optional[Float[Tensor, "B H 1"]] = None,
-    ) -> Float[Tensor, "B H 1"]:
-        h, gate = self.hg(x).chunk(2, dim=1)
-        h = g(h)
-        gate = gate.sigmoid()
-
-        if prev_hidden is None:
-            return h * gate
-        return th.lerp(prev_hidden, h, gate)
-
-    def forward(
-        self,
-        x: Float[Tensor, "B D L"],
-        prev_hidden: Optional[Float[Tensor, "B H 1"]] = None,
-    ) -> Float[Tensor, "B H L"]:
-        h, gate = self.hg(x).chunk(2, dim=1)
-
-        log_coeffs = -F.softplus(gate)
-        log_values = -F.softplus(-gate) + log_g(h)
-
-        if prev_hidden is not None:
-            log_values = th.cat([prev_hidden.log(), log_values], dim=2)
-            log_coeffs = F.pad(log_coeffs, (0,0, 0,0, 1,0))
-
-        # heinsen associative scan (log-space)
-        a_star = log_coeffs.cumsum(dim=2)
-        log_h0_plus_b_star = (log_values - a_star).logcumsumexp(dim=2)
-        log_h = a_star + log_h0_plus_b_star
-        h = log_h.exp()
-
-        return h[:,:,-x.size(2):]
+    # heinsen associative scan (log-space)
+    a_star = log_coeffs.cumsum(dim=2)
+    log_h0_plus_b_star = (log_values - a_star).logcumsumexp(dim=2)
+    log_h = a_star + log_h0_plus_b_star
+    
+    return log_h.exp()
 
 class minGRULayer(nn.Module):
     def __init__(self, dim: int):
@@ -60,11 +26,11 @@ class minGRULayer(nn.Module):
         h_dim = dim // 2
         assert h_dim * 2 == dim
 
-        self.fore = minGRU(dim, h_dim)
-        self.back = minGRU(dim, h_dim)
-        self.out = nn.Conv1d(dim, dim, 1)
+        self.fore = nn.Conv1d(dim, dim, 1)
+        self.back = nn.Conv1d(dim, dim, 1)
 
     def forward(self, x: Float[Tensor, "B D L"]) -> Float[Tensor, "B D L"]:
-        fore = self.fore(x)
-        back = self.back(x.flip(2)).flip(2)
-        return self.out(th.cat([fore, back], dim=1))
+        return th.cat([
+            min_gru(self.fore(x)),
+            min_gru(self.back(x.flip(2))).flip(2),
+        ], dim=1)
