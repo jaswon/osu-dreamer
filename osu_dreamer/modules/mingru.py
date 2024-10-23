6b4c524f1c446eac11ddd5abf295a073025ff8d4
jason <jason@jwon.xyz>
2024-10-23 15:25:56 +0000

    minGRU: layer caller owns hg

--- osu_dreamer/modules/mingru.py
+++ osu_dreamer/modules/mingru.py
@@ -5,8 +5,6 @@ import torch as th
 from torch import nn, Tensor
 import torch.nn.functional as F
 
-from osu_dreamer.modules.modconv import ModulatedConv1d
-
 log_g = lambda x: th.where(x >= 0, th.log(F.relu(x)+1), x)
 
 def min_gru(hg: Float[Tensor, "B H L"]) -> Float[Tensor, "B H/2 L"]:
@@ -22,32 +20,12 @@ def min_gru(hg: Float[Tensor, "B H L"]) -> Float[Tensor, "B H/2 L"]:
     
     return log_h.exp()
 
-class minGRULayer(nn.Module):
-    def __init__(self, dim: int):
-        super().__init__()
-        h_dim = dim // 2
-        assert h_dim * 2 == dim
+def min_gru_bidirectional(x: Float[Tensor, "B H L"]) -> Float[Tensor, "B H/2 L"]:
+    fore, back = x.chunk(2, dim=1)
+    return th.cat([ min_gru(fore), min_gru(back.flip(2)).flip(2) ], dim=1)
 
-        self.fore = nn.Conv1d(dim, dim, 1)
-        self.back = nn.Conv1d(dim, dim, 1)
-
-    def forward(self, x: Float[Tensor, "B D L"]) -> Float[Tensor, "B D L"]:
-        return th.cat([
-            min_gru(self.fore(x)),
-            min_gru(self.back(x.flip(2))).flip(2),
-        ], dim=1)
-
-class minGRUModLayer(nn.Module):
-    def __init__(self, dim: int, c_dim: int):
-        super().__init__()
-        h_dim = dim // 2
-        assert h_dim * 2 == dim
-
-        self.fore = ModulatedConv1d(dim, dim, c_dim)
-        self.back = ModulatedConv1d(dim, dim, c_dim)
-
-    def forward(self, x: Float[Tensor, "B D L"], c: Float[Tensor, "B C"]) -> Float[Tensor, "B D L"]:
-        return th.cat([
-            min_gru(self.fore((x,c))),
-            min_gru(self.back((x.flip(2),c))).flip(2),
-        ], dim=1)
+class minGRU2(nn.Module):
+    def __init__(self): super().__init__()
+    
+    def forward(self, x: Float[Tensor, "B H L"]) -> Float[Tensor, "B H/2 L"]:
+        return min_gru_bidirectional(x)
