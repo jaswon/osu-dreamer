b890b8667b8cc1d70489591f1dfc13d1c9022ee5
jason <jason@jwon.xyz>
2024-10-23 02:59:42 +0000

    minGRU: modulated conv

--- osu_dreamer/modules/mingru.py
+++ osu_dreamer/modules/mingru.py
@@ -5,6 +5,8 @@ import torch as th
 from torch import nn, Tensor
 import torch.nn.functional as F
 
+from osu_dreamer.modules.modconv import ModulatedConv1d
+
 log_g = lambda x: th.where(x >= 0, th.log(F.relu(x)+1), x)
 
 def min_gru(hg: Float[Tensor, "B H L"]) -> Float[Tensor, "B H/2 L"]:
@@ -33,4 +35,19 @@ class minGRULayer(nn.Module):
         return th.cat([
             min_gru(self.fore(x)),
             min_gru(self.back(x.flip(2))).flip(2),
+        ], dim=1)
+
+class minGRUModLayer(nn.Module):
+    def __init__(self, dim: int, c_dim: int):
+        super().__init__()
+        h_dim = dim // 2
+        assert h_dim * 2 == dim
+
+        self.fore = ModulatedConv1d(dim, dim, c_dim)
+        self.back = ModulatedConv1d(dim, dim, c_dim)
+
+    def forward(self, x: Float[Tensor, "B D L"], c: Float[Tensor, "B C"]) -> Float[Tensor, "B D L"]:
+        return th.cat([
+            min_gru(self.fore((x,c))),
+            min_gru(self.back((x.flip(2),c))).flip(2),
         ], dim=1)
