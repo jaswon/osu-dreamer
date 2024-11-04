12955d24c7fe661372290a2e5d11d898e28c29ec
jason <jason@jwon.xyz>
2024-11-04 20:15:57 +0000

    minGRU: tidy

--- osu_dreamer/modules/mingru.py
+++ osu_dreamer/modules/mingru.py
@@ -5,12 +5,14 @@ import torch as th
 from torch import nn, Tensor
 import torch.nn.functional as F
 
-def min_gru(hg: Float[Tensor, "B H ... L"]) -> Float[Tensor, "B H/2 ... L"]:
-    h, gate = hg.chunk(2, dim=1)
-    h_tilde = th.where(h < 0, h, th.log(F.relu(h)+1))
+def min_gru(
+    h: Float[Tensor, "B H ... L"], 
+    g: Float[Tensor, "B H ... L"],
+) -> Float[Tensor, "B H ... L"]:
+    log_h_tilde = th.where(h < 0, h, th.log(F.relu(h)+1)) # h_tilde = 1+ELU(x)
 
-    log_coeffs = -F.softplus(gate)
-    log_values = -F.softplus(-gate) + h_tilde
+    log_coeffs = -F.softplus(g)
+    log_values = -F.softplus(-g) + log_h_tilde
 
     # heinsen associative scan (log-space)
     a_star = log_coeffs.cumsum(dim=-1)
@@ -21,7 +23,10 @@ def min_gru(hg: Float[Tensor, "B H ... L"]) -> Float[Tensor, "B H/2 ... L"]:
 
 def min_gru_bidirectional(x: Float[Tensor, "B H ... L"]) -> Float[Tensor, "B H/2 ... L"]:
     fore, back = x.chunk(2, dim=1)
-    return th.cat([ min_gru(fore), min_gru(back.flip(2)).flip(2) ], dim=1)
+    return th.cat([
+        min_gru(*fore.chunk(2, dim=1)),
+        min_gru(*back.flip(2).chunk(2, dim=1)).flip(2),
+    ], dim=1)
 
 class minGRU2(nn.Module):
     def __init__(self): super().__init__()
