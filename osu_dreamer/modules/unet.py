28d74e46467855849d14f6ec824cf8d2e1c83ef9
jason <jason@jwon.xyz>
2024-11-05 23:52:11 +0000

    unet: remove conditioning

--- osu_dreamer/modules/unet.py
+++ osu_dreamer/modules/unet.py
@@ -9,105 +9,97 @@ import torch.nn.functional as F
 import scipy.signal as signal
 from einops import repeat
 
-from osu_dreamer.modules.film import FiLM
+class LPFUpsample(nn.Module):
+    def __init__(
+        self,
+        dim: int, 
+        scale: int, 
+        kernel_size: int = 17,
+    ):
+        assert kernel_size % 2 == 1
+        super().__init__()
+        
+        self.dim = dim
+        self.pad = kernel_size // 2
+        self.scale = scale
 
-def make_lpf(dim: int, scale: int, kernel_size: int = 17):
-    assert kernel_size % 2 == 1
+        # sinc filter w/kaiser window
+        beta = signal.kaiser_beta(signal.kaiser_atten(kernel_size, scale ** -1))
+        kaiser = th.tensor(signal.windows.kaiser(kernel_size, beta))
+        X = th.arange(kernel_size) - kernel_size // 2
+        K = th.sinc(X/scale) * kaiser
+        K = K / K.sum()
+        self.K = K
 
-    # sinc filter w/kaiser window
-    beta = signal.kaiser_beta(signal.kaiser_atten(kernel_size, scale ** -1))
-    kaiser = th.tensor(signal.windows.kaiser(kernel_size, beta))
-    X = th.arange(kernel_size) - kernel_size // 2
-    K = th.sinc(X/scale) * kaiser
-    K = K / K.sum()
-
-    def lpf(x: Float[Tensor, "B D L"]) -> Float[Tensor, "B D L"]:
+    def forward(self, x: Float[Tensor, "B D L"]) -> Float[Tensor, "B D L*{self.scale}"]:
         return F.conv1d(
-            x, repeat(K.to(x), 'k -> d 1 k', d=dim),
-            padding=kernel_size // 2,
-            groups=dim,
+            repeat(x, 'b d l -> b d (l s)', s=self.scale), 
+            repeat(self.K.to(x), 'k -> d 1 k', d=self.dim),
+            padding=self.pad,
+            groups=self.dim,
         )
-
-    return lpf
 
 class UNetLayer(nn.Module):
     def __init__(
         self,
         dim: int,
-        t_dim: int,
         scale: int,
         pre: nn.Module,
         middle: nn.Module,
     ):
         super().__init__()
-
         self.pre = pre
-            
-        self.norm1 = FiLM(dim, t_dim)
         self.skip = nn.Conv1d(dim, dim, scale*2-1, 1, scale-1)
-
         self.down = nn.AvgPool1d(scale, scale)
-
-        self.norm2 = FiLM(dim, t_dim)
         self.middle = middle
-        
-        lpf = make_lpf(dim, scale)
-        self.up = lambda x: lpf(repeat(x, 'b d l -> b d (l s)', s=scale))
-        
-        self.norm3 = FiLM(2*dim, t_dim)
+        self.up = LPFUpsample(dim, scale)
         self.gate = nn.Sequential(
             nn.Conv1d(2*dim, dim, 1),
             nn.SiLU(),
             nn.Conv1d(dim, dim, 1),
         )
-        
-        self.norm4 = FiLM(2*dim, t_dim)
         self.out = nn.Conv1d(2*dim, dim, 1)
 
     def forward(
         self,
         x: Float[Tensor, "B D L"],
-        t: Float[Tensor, "B T"],
     ) -> Float[Tensor, "B D L"]:
-        x = self.pre(x, t)
-        skip = self.skip(self.norm1(x, t))
+        x = self.pre(x)
+        skip = self.skip(x)
         down = self.down(x)
-        middle = self.middle(self.norm2(down, t), t)
+        middle = self.middle(down)
         up = self.up(middle)
-        gate = self.gate(self.norm3(th.cat([up, skip], dim=1), t))
+        gate = self.gate(th.cat([up, skip], dim=1))
         mix = skip * gate
-        return self.out(self.norm4(th.cat([up, mix], dim=1), t))
+        return self.out(th.cat([up, mix], dim=1))
         
 
 class UNet(nn.Module):
     def __init__(
         self,
         dim: int,
-        t_dim: int,
         scales: list[int],
-        middle: nn.Module,
         block: Callable[[], nn.Module],
     ):
         super().__init__()
 
-        net = middle
+        net = block()
         self.chunk_size = 1
         for scale in scales:
             self.chunk_size *= scale
-            net = UNetLayer(dim, t_dim, scale, block(), net)
+            net = UNetLayer(dim, scale, block(), net)
 
         self.net = net
 
     def forward(
         self,
         x: Float[Tensor, "B D L"],
-        t: Float[Tensor, "B T"],
     ) -> Float[Tensor, "B D L"]:
         padding = (self.chunk_size-x.size(-1)%self.chunk_size)%self.chunk_size
         if padding > 0:
             x = F.pad(x, (0, padding))
 
-        x = self.net(x, t)
+        x = self.net(x)
 
         if padding > 0:
             x = x[...,:-padding]
