c1860ed6e94797dfe5ff55c031b9f9be7ae69de6
jason <jason@jwon.xyz>
2024-11-15 20:23:11 +0000

    audio: mel spectrogram, optional librosa

--- osu_dreamer/data/load_audio.py
+++ osu_dreamer/data/load_audio.py
@@ -2,18 +2,17 @@
 from jaxtyping import Float
 
 import numpy as np
+from numpy import ndarray
 
 import ffmpeg
-import librosa
 
 # audio processing constants
+A_DIM = 96
 SR = 22000
 MS_PER_FRAME = 8
 HOP_LEN = (SR // 1000) * MS_PER_FRAME
-
-N_OCTAVES = 8
-OCTAVE_BINS = 12
-A_DIM = N_OCTAVES * OCTAVE_BINS
+N_FFT = 2048
+eps = 1e-10
 
 FrameTimes = Float[np.ndarray, "L"]
 
@@ -23,24 +22,53 @@ def get_frame_times(num_frames: int) -> FrameTimes:
     samples = frames * HOP_LEN + N_FFT // 2
     return samples / SR * 1000
 
-eps = 1e-10
-fmin = librosa.note_to_hz('C0')
+def load_audio(file_name):
+    return compute_repr(read_file(file_name))
 
-def load_audio(audio_file):
+def read_file(file_name) -> Float[ndarray, "N"]:
     buf = ( ffmpeg
-        .input(audio_file)
+        .input(file_name)
         .output('-', format='s16le', acodec='pcm_s16le', ac=1, ar=str(SR))
         .overwrite_output()
         .run(quiet=True)
     )[0]
-    wave = np.frombuffer(buf, dtype=np.int16).astype(np.float32) / float(1 << 15)
+    return np.frombuffer(buf, dtype=np.int16).astype(np.float32) / float(1 << 15)
 
-    # compute time-frequency representation
-    return np.log(eps + np.abs(librosa.vqt(
-        y=wave,
-        sr=SR,
-        hop_length=HOP_LEN,
-        fmin=fmin,
-        n_bins=A_DIM,
-        bins_per_octave=OCTAVE_BINS,
-    )))
+def compute_repr(wave: Float[ndarray, "N"]) -> Float[ndarray, "M L"]:
+    try:
+        import librosa
+        mel = librosa.feature.melspectrogram(
+            y=wave, 
+            sr=SR, 
+            n_fft=N_FFT, 
+            hop_length=HOP_LEN, 
+            n_mels=A_DIM, 
+            htk=True,
+        )
+    except ImportError:
+        mel = compute_scipy_mel(wave)
+    return np.log(eps + mel)
+    
+def compute_scipy_mel(wave: Float[ndarray, "N"]) -> Float[ndarray, "M L"]:
+    import scipy.signal
+    from scipy.signal.windows import hann
+
+    S = scipy.signal.ShortTimeFFT(hann(N_FFT, sym=False), hop=HOP_LEN, fs=SR)
+    spec = S.spectrogram(wave)[:,S.lower_border_end[1]-1:S.p_min-1]
+    return np.einsum('fl,mf->ml', spec, mel_filter_banks(S.f))
+
+# mel scale (O'Shaughnessy 1987)
+mel2hz = lambda m: 700 * np.expm1(m/1127)
+hz2mel = lambda f: 1127 * np.log1p(f/700)
+
+def mel_filter_banks(fft_freqs: Float[ndarray, "F"]) -> Float[ndarray, "M F"]:
+    mel_f = mel2hz(np.linspace(hz2mel(0), hz2mel(SR / 2.), A_DIM+2))
+
+    fdiff = np.diff(mel_f)[:,None] # M+1
+    ramps = np.subtract.outer(mel_f, fft_freqs) # M+2 F
+    weights = np.maximum(0, np.minimum(-ramps[:-2] / fdiff[:-1], ramps[2:] / fdiff[1:]))
+
+    # divide mel weights by band width (area normalization)
+    weights *= 2.0 / (mel_f[2:] - mel_f[:-2])[:,None]
+
+    return weights
--- osu_dreamer/data/plot.py
+++ osu_dreamer/data/plot.py
@@ -3,50 +3,49 @@ from contextlib import contextmanager
 
 from jaxtyping import Float
 
+import numpy as np
 from numpy import ndarray
-import librosa
 
 import matplotlib.pyplot as plt
 
 @contextmanager
 def plot_signals(audio: Float[ndarray, "A L"], signals: list[Float[ndarray, "X L"]]):
-        margin, margin_left = .1, .5
-        height_ratios = [.8] + [.6] * len(signals)
-        plots_per_row = len(height_ratios)
-        w, h = audio.shape[-1] * .01, sum(height_ratios) * .4
+    margin, margin_left = .1, .5
+    height_ratios = [.8] + [.6] * len(signals)
+    plots_per_row = len(height_ratios)
+    w, h = audio.shape[-1] * .01, sum(height_ratios) * .4
 
-        # split plot across multiple rows
-        split = ((w/h)/(3/5)) ** .5 # 3 wide by 5 tall aspect ratio
-        split = int(split + 1)
-        w = w // split
-        h = h * split
-        height_ratios = height_ratios * split
-        
-        fig, all_axs = plt.subplots(
-            len(height_ratios), 1,
-            figsize=(w, h),
-            sharex=True,
-            gridspec_kw=dict(
-                height_ratios=height_ratios,
-                hspace=.1,
-                left=margin_left/w,
-                right=1-margin/w,
-                top=1-margin/h,
-                bottom=margin/h,
-            )
+    # split plot across multiple rows
+    split = ((w/h)/(3/5)) ** .5 # 3 wide by 5 tall aspect ratio
+    split = int(split + 1)
+    w = w // split
+    h = h * split
+    height_ratios = height_ratios * split
+    
+    fig, all_axs = plt.subplots(
+        len(height_ratios), 1,
+        figsize=(w, h),
+        sharex=True,
+        gridspec_kw=dict(
+            height_ratios=height_ratios,
+            hspace=.1,
+            left=margin_left/w,
+            right=1-margin/w,
+            top=1-margin/h,
+            bottom=margin/h,
         )
+    )
 
-        win_len = audio.shape[-1] // split
-        for i in range(split):
-            ax1, *axs = all_axs[i * plots_per_row: (i+1) * plots_per_row]
-            sl = (..., slice(i * win_len, (i+1) * win_len))
+    win_len = audio.shape[-1] // split
+    for i in range(split):
+        ax1, *axs = all_axs[i * plots_per_row: (i+1) * plots_per_row]
+        sl = (..., slice(i * win_len, (i+1) * win_len))
+        ax1.imshow(audio[sl], origin="lower", aspect='auto')
+        
+        for (i, sample), ax in zip(enumerate(signals), axs):
+            ax.margins(x=0)
+            for ch in sample[sl]:
+                ax.plot(ch)
 
-            ax1.imshow(librosa.power_to_db(audio[sl]), origin="lower", aspect='auto')
-            
-            for (i, sample), ax in zip(enumerate(signals), axs):
-                ax.margins(x=0)
-                for ch in sample[sl]:
-                    ax.plot(ch)
-
-        yield fig
-        plt.close(fig)
+    yield fig
+    plt.close(fig)
