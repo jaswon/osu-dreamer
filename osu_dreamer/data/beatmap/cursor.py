9183fab1759abc92f972cfff57e2d5a627442697
jason <jason@jwon.xyz>
2024-11-18 21:06:38 +0000

    encode: vectorize cursor encoding

--- osu_dreamer/data/beatmap/cursor.py
+++ osu_dreamer/data/beatmap/cursor.py
@@ -31,37 +31,33 @@ def cursor_signal(bm: Beatmap, frame_times: FrameTimes) -> CursorSignal:
     # compute duration a hit object stays on screen from approach rate
     preempt = 1200 + (120 if bm.ar <= 5 else 150) * (5-bm.ar)
 
-    start = Circle(0, True, 256, 192)
-    hos: list[HitObject] = [start, *bm.hit_objects]
+    hos: list[HitObject] = [Circle(0, True, 256, 192), *bm.hit_objects]
 
-    pos: list[Float[ndarray, "2"]] = []
+    sig = np.zeros((frame_times.shape[0], 2))
 
     for cur, nxt in zip(hos, hos[1:] + [None]):
 
         # hit current object
+        cur_i = (frame_times >= cur.t) & (frame_times < cur.end_time())
         if isinstance(cur, Spinner):
-            cur_count = np.sum((frame_times >= cur.t) & (frame_times < cur.end_time()))
-            pos.extend(cur.start_pos()[None].repeat(cur_count, axis=0))
+            sig[cur_i] = cur.start_pos()
         elif isinstance(cur, Slider):
-            cur_t = frame_times[(frame_times >= cur.t) & (frame_times < cur.end_time())]
-            cur_f = (cur_t - cur.t) % (cur.slide_duration * 2) / cur.slide_duration
-            if len(cur_f) > 0:
-                pos.extend(cur.lerp(np.where(cur_f < 1, cur_f, 2 - cur_f)))
+            cur_f = (frame_times[cur_i] - cur.t) % (cur.slide_duration * 2) / cur.slide_duration
+            sig[cur_i] = cur.lerp(np.where(cur_f < 1, cur_f, 2 - cur_f))
 
         if nxt is None:
             # end of map
-            map_end_count = np.sum(frame_times >= cur.end_time())
-            pos.extend(cur.end_pos()[None].repeat(map_end_count, axis=0))
+            sig[frame_times >= cur.end_time()] = cur.end_pos()
             break
 
         # current object hit, wait for next object to appear
-        wait_count = np.sum((frame_times >= cur.end_time()) & (frame_times < nxt.t - preempt))
-        pos.extend(cur.end_pos()[None].repeat(wait_count, axis=0))
+        wait_t = (frame_times >= cur.end_time()) & (frame_times < nxt.t - preempt)
+        sig[wait_t] = cur.end_pos()
 
         # approach next hit object
-        start_time = max(cur.end_time(), nxt.t - preempt)
-        approach_t = frame_times[(frame_times >= start_time) & (frame_times < nxt.t)]
-        approach_f = (approach_t - start_time) / (nxt.t - start_time)
-        pos.extend((1 - approach_f[:,None]) * cur.end_pos() + approach_f[:,None] * nxt.start_pos())
+        approach_start_time = max(cur.end_time(), nxt.t - preempt)
+        approach_i = (frame_times >= approach_start_time) & (frame_times < nxt.t)
+        approach_f = (frame_times[approach_i] - approach_start_time) / (nxt.t - approach_start_time)
+        sig[approach_i] = (1 - approach_f[:,None]) * cur.end_pos() + approach_f[:,None] * nxt.start_pos()
         
-    return (np.array(pos) / np.array([256,192])).T - 1
+    return (np.array(sig) / np.array([256,192])).T - 1
