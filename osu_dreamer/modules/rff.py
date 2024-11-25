2ba5bd294902550adec998cd16b3376ce73a7082
jason <jason@jwon.xyz>
2024-11-25 22:59:29 +0000

    rff: cosine only, no conv

--- osu_dreamer/modules/rff.py
+++ osu_dreamer/modules/rff.py
@@ -1,28 +1,19 @@
 
+from jaxtyping import Float
+
 import torch as th
-from torch import nn
-import torch.nn.functional as F
-
-from einops import rearrange
-
+from torch import nn, Tensor
+    
 class RandomFourierFeatures(nn.Module):
     def __init__(
         self,
         dim: int, 
         n_feats: int,
-        scale: float = 10.,
-        conv1x1: bool = False,
     ):
         super().__init__()
-        self.conv1x1 = conv1x1
+        self.register_buffer('f', th.randn(dim, n_feats))
+        self.register_buffer('p', th.rand(n_feats))
 
-        d = n_feats//2
-        assert d * 2 == n_feats, '`n_feats` must be even'
-        self.register_buffer('W', th.randn(dim, d) * scale)
-
-    def forward(self, x):
-        if self.conv1x1:
-            theta = F.conv1d(x * 2 * th.pi, rearrange(self.W, 'i o -> o i 1'))
-        else:
-            theta = (x * 2 * th.pi) @ self.W
-        return th.cat([theta.sin(), theta.cos()], dim=1 if self.conv1x1 else -1)
+    def forward(self, x: Float[Tensor, "B C"]) -> Float[Tensor, "B N"]:
+        theta = 2 * th.pi * (x @ self.f + self.p)
+        return theta.cos()
