cc3034b5842de7a10b29ef4af7f1b9235751ac07
jason <jason@jwon.xyz>
2024-11-30 17:26:44 +0000

    audio_features: no conv2d

--- osu_dreamer/diffusion_model/audio_features.py
+++ osu_dreamer/diffusion_model/audio_features.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 from jaxtyping import Float
 
+import torch as th
 from torch import nn, Tensor 
 
 from osu_dreamer.data.load_audio import A_DIM
@@ -12,10 +13,8 @@ import osu_dreamer.modules.mp as MP
 @dataclass
 class AudioFeatureArgs:
     dim: int
-    scales: list[int]
-    conv_expand: int
-    seq_depth: int
-    seq_expand: int
+    depth: int
+    expand: int
 
 class AudioFeatures(nn.Module):
     def __init__(
@@ -24,56 +23,20 @@ class AudioFeatures(nn.Module):
     ):
         super().__init__()
 
-        in_dim = args.dim // 2**len(args.scales)
-        assert 2**len(args.scales) * in_dim == args.dim
+        self.proj_in = MP.Conv1d(A_DIM+1, args.dim, 1)
 
-        self.proj_in = nn.Sequential(
-            nn.Unflatten(1, (1, -1)), 
-            nn.Conv2d(1, in_dim, 1),
-        )
-
-        class conv_layer(nn.Module):
-            def __init__(self, d: int, s: int):
-                super().__init__()
-                h = d*args.conv_expand
-                self.res = nn.Sequential(
-                    MP.SiLU(),
-                    MP.Conv2d(d, h, 1),
-                    MP.Conv2d(h, h, *zip((5,1,2), (3,1,1)), groups=h),
-                    MP.SiLU(),
-                    MP.Conv2d(h, d, 1),
-                )
-                self.down = nn.Sequential(
-                    nn.AvgPool2d((s,1),(s,1)),
-                    MP.Conv2d(d, d*2, 1),
-                )
-        
-            def forward(self, x: Float[Tensor, "B d F L"]) -> Float[Tensor, "B D f L"]:
-                x = MP.pixel_norm(x)
-                h = MP.add(x, self.res(x), t=.3)
-                return self.down(h)
-            
-        self.conv_layers = nn.ModuleList()
-        size = 1
-        d = in_dim
-        for s in args.scales:
-            self.conv_layers.append(conv_layer(d, s))
-            size *= s
-            d *= 2
-        assert A_DIM == size
-
-        H = d * args.seq_expand
-        class seq_layer(nn.Module):
+        H = args.dim * args.expand
+        class layer(nn.Module):
             def __init__(self):
                 super().__init__()
-                self.hg = MP.Conv1d(d, H*2, 1)
+                self.hg = MP.Conv1d(args.dim, H*2, 1)
                 self.net = nn.Sequential(
                     MP.SiLU(),
                     MP.Conv1d(H, H, 3,1,1, groups=H),
                     MP.SiLU(),
                     MP.minGRU2(H),
                 )
-                self.out = MP.Conv1d(H, d, 1)
+                self.out = MP.Conv1d(H, args.dim, 1)
 
             def forward(self, x: Float[Tensor, "B X L"]) -> Float[Tensor, "B X L"]:
                 x = MP.pixel_norm(x)
@@ -81,16 +44,14 @@ class AudioFeatures(nn.Module):
                 h = self.net(h) * MP.silu(g)
                 return MP.add(x, self.out(h), t=.3)
             
-        self.seq_layers = nn.ModuleList([ seq_layer() for _ in range(args.seq_depth) ])
+        self.layers = nn.ModuleList([ layer() for _ in range(args.depth) ])
 
     def forward(
         self,
         audio: Float[Tensor, str(f"B {A_DIM} L")],
     ) -> Float[Tensor, "B D L"]:
+        audio = MP.cat([audio, th.ones_like(audio[:,:1,:])], dim=1)
         h = self.proj_in(audio)
-        for conv_layer in self.conv_layers:
-            h = conv_layer(h)
-        h = h.flatten(2)
-        for seq_layer in self.seq_layers:
-            h = seq_layer(h)
+        for layer in self.layers:
+            h = layer(h)
         return MP.silu(h)
