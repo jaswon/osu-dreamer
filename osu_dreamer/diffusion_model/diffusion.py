6d29d3a9cfd7843ef9d5d86e905a682ce1ada3b3
jason <jason@jwon.xyz>
2024-12-01 21:02:35 +0000

    diffusion: tidy tensor shapes

--- osu_dreamer/diffusion_model/diffusion.py
+++ osu_dreamer/diffusion_model/diffusion.py
@@ -11,11 +11,10 @@ from einops import repeat
 
 import osu_dreamer.modules.mp as MP
 
-U = Float[Tensor, "B"]              # loss weight, uncertainty
+B = Float[Tensor, "B"]              # noise level, loss weight, uncertainty
 F = Float[Tensor, "B F"]            # noise level features
-T = Float[Tensor, "B 1 1"]          # diffusion step
 X = Float[Tensor, "B D N"]          # sequence
-Denoiser = Callable[[ X, T ], X]    # p(x0 | xt, t)
+Denoiser = Callable[[ X, B ], X]    # p(x0 | xt, t)
 
 @dataclass
 class DiffusionArgs:
@@ -43,7 +42,7 @@ class Diffusion(nn.Module):
         self.noise_level_features = MP.RandomFourierFeatures(1, args.noise_level_features)
         self.proj_u = MP.Linear(args.noise_level_features, 1)
 
-    def pred_x0(self, model: Denoiser, x_t: X, std: T) -> tuple[X,U]:
+    def pred_x0(self, model: Denoiser, x_t: X, std: B) -> tuple[X,B]:
         """https://arxiv.org/pdf/2206.00364.pdf#section.5"""
 
         sq_sum = std ** 2 + self.std_data ** 2
@@ -51,22 +50,24 @@ class Diffusion(nn.Module):
         c_skip = self.std_data ** 2 / sq_sum
         c_out = std * self.std_data / hyp
         c_in = 1 / hyp
-        c_noise = th.log(std)
+        c_noise = th.log(std) / 4
 
-        nlf = self.noise_level_features(c_noise[:,:,0])
-        model_out = model(c_in * x_t, nlf)
-        pred_x0 = c_skip * x_t + c_out * model_out
-        return pred_x0, self.proj_u(nlf)[:,0]
+        nlf = self.noise_level_features(c_noise[:,None])
+        u = self.proj_u(nlf)[:,0]
+        model_out = model(c_in[:,None,None] * x_t, nlf)
+        pred_x0 = c_skip[:,None,None] * x_t + c_out[:,None,None] * model_out
+        return pred_x0, u
 
-    def training_sample(self, model: Denoiser, x0: X) -> tuple[X,U,U]:
+    def training_sample(self, model: Denoiser, x0: X) -> tuple[X,B,B]:
         """sample denoised predictions and per-batch loss weights"""
         b = x0.size(0)
         u = (th.arange(b) + th.rand(1)) / b # low discrepancy sampling
-        t = self.std_noise(u)[:,None,None].to(x0.device)
+        t = self.std_noise(u).to(x0.device)
         loss_weight = (t ** 2 + self.std_data ** 2) / (t * self.std_data) ** 2
-        x_t = x0 + th.randn_like(x0) * t
+        x_t = x0 + th.randn_like(x0) * t[:,None,None]
 
-        return *self.pred_x0(model, x_t, t), loss_weight[:,0,0]
+        pred_x0, u = self.pred_x0(model, x_t, t)
+        return pred_x0, u, loss_weight
     
     @th.no_grad()
     def sample(
@@ -85,14 +86,13 @@ class Diffusion(nn.Module):
         
         sigmas = self.std_noise(th.linspace(0, 1, num_steps))
         sigmas = th.tensor([*sigmas.tolist(), 0], device=z.device)
-        sigmas = repeat(sigmas, 's -> s b 1 1', b = z.size(0))
 
         loop = zip(sigmas[:-1], sigmas[1:])
         if show_progress:
             from tqdm import tqdm
             loop = tqdm(loop, total=num_steps)
 
-        def dx_dt(x: X, t: T):
+        def dx_dt(x: X, t: Float[Tensor, ""]):
             """
             https://arxiv.org/pdf/2011.13456.pdf#section.5
             
@@ -101,14 +101,14 @@ class Diffusion(nn.Module):
                 = ∇logp(xt) + ∇logp(y|xt)
                 = (pred_x0 - xt) / t**2 + ∇logp(y|xt)
             """
-            x0_hat = self.pred_x0(denoiser, x, t)[0]
-            score = (x0_hat - x) / t ** 2
+            pred_x0, _ = self.pred_x0(denoiser, x, repeat(t, '-> b', b=x.size(0)))
+            score = (pred_x0 - x) / t ** 2
             return -t * score
 
-        x_t = z * (sigmas[0,0,0,0] ** 2 + self.std_data ** 2) ** .5
+        x_t = z * (sigmas[0] ** 2 + self.std_data ** 2) ** .5
         for t_cur, t_nxt in loop:
             # increase noise temporarily
-            gamma = min(S_churn / num_steps, 2**.5 - 1) if S_tmin <= t_cur[0,0,0] <= S_tmax else 0
+            gamma = min(S_churn / num_steps, 2**.5 - 1) if S_tmin <= t_cur <= S_tmax else 0
             t_hat = t_cur + gamma * t_cur
             x_hat = x_t + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * th.randn_like(x_t)
 
@@ -117,7 +117,7 @@ class Diffusion(nn.Module):
             x_t = x_hat + (t_nxt - t_hat) * d_cur
 
             # 2nd order correction (Huen's method)
-            if t_nxt[0,0,0] > 0:
+            if t_nxt > 0:
                 d_prime = dx_dt(x_t, t_nxt)
                 x_t = x_hat + 0.5 * (t_nxt - t_hat) * (d_cur + d_prime)
 
