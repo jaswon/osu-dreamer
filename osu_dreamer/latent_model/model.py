f35db0286edd24bb06ad8aa9f3ad5e2cd77b24ec
jason <jason@jwon.xyz>
2024-12-17 20:38:00 +0000

    latent: encoders have built in chunk padding

--- osu_dreamer/latent_model/model.py
+++ osu_dreamer/latent_model/model.py
@@ -49,7 +49,7 @@ class Model(pl.LightningModule):
         self.save_hyperparameters()
         self.x_dim = args.x_dim
         self.a_dim = args.a_dim
-        self.chunk_size = 1 << args.depth
+        self.chunk_size = chunk_size = 1 << args.depth
 
         # training params
         self.step_ref = step_ref
@@ -58,13 +58,27 @@ class Model(pl.LightningModule):
 
         # model
 
+        class ChunkPad(nn.Module):
+            def __init__(self, pad_value: float = 0.):
+                super().__init__()
+                self.pad_value = pad_value
+                self.chunk_size = chunk_size
+        
+            def forward(self, x: Float[Tensor, "B D iL"]) -> Float[Tensor, "B D oL"]:
+                pad = (self.chunk_size - x.size(-1)%self.chunk_size) % self.chunk_size
+                if pad > 0:
+                    x = F.pad(x, (0,pad), value=self.pad_value)
+                return x
+
         self.audio_encoder = nn.Sequential(
+            ChunkPad(pad_value=-1.),
             MP.Conv1d(A_DIM, args.h_dim, 1),
             MP.PixelNorm(),
             Encoder(args.h_dim, args.depth, args.blocks_per_depth),
             MP.Conv1d(args.h_dim, args.a_dim, 1),
         )
         self.chart_encoder = nn.Sequential(
+            ChunkPad(),
             MP.Conv1d(X_DIM, args.h_dim, 1),
             MP.PixelNorm(),
             Encoder(args.h_dim, args.depth, args.blocks_per_depth),
@@ -79,23 +93,15 @@ class Model(pl.LightningModule):
             MP.Gain(),
         )
 
-    def _padding(self, L: int):
-        return (self.chunk_size-L%self.chunk_size)%self.chunk_size
-
     def forward(
         self,
         audio: Float[Tensor, str(f"B {A_DIM} L")],
         chart: Float[Tensor, str(f"B {X_DIM} L")],
         labels: Float[Tensor, str(f"B {NUM_LABELS}")],
     ) -> tuple[Float[Tensor, ""], dict[str, Float[Tensor, ""]]]:
-        pad = self._padding(audio.size(-1))
-        if pad > 0:
-            chart = F.pad(chart, (0,pad))
-            audio = F.pad(audio, (0,pad), value=-1)
-
-        z = self.chart_encoder(chart)
-        q_z, vq_loss = self.vq(z)
-        pred_chart = self.decoder(MP.cat([q_z, self.audio_encoder(audio)], dim=1))
+        za = self.audio_encoder(audio)
+        q_zx, vq_loss = self.vq(self.chart_encoder(chart))
+        pred_chart = self.decoder(MP.cat([q_zx, za], dim=1))[:,:,:chart.size(-1)]
         recon_loss = th.mean((chart - pred_chart) ** 2)
         bound_loss = th.mean((pred_chart.abs().clamp(min=1) - 1) ** 2)
 
@@ -148,17 +154,12 @@ class Model(pl.LightningModule):
 
     def plot_sample(self, b: Batch):
         a, x, _ = b
-        
-        padding = (self.chunk_size-x.size(-1)%self.chunk_size)%self.chunk_size
-        if padding > 0:
-            x = F.pad(x, (0, padding))
-            a = F.pad(a, (0,padding), value=-1)
 
         with th.no_grad():
             za = self.audio_encoder(a)
             q_zx = self.vq(self.chart_encoder(x))[0]
-            x_hat = self.decoder(MP.cat([q_zx, za], dim=1))
-            plots = [ x[0].cpu().numpy() for x in [ x, x_hat, repeat(q_zx, 'b d l -> b d (l r)', r=self.chunk_size) ] ]
+            x_hat = self.decoder(MP.cat([q_zx, za], dim=1))[:,:,:x.size(-1)]
+            plots = [ x[0].cpu().numpy() for x in [ x, x_hat, repeat(q_zx, 'b d l -> b d (l r)', r=self.chunk_size)[:,:,:x.size(-1)] ] ]
 
         with plot_signals(a[0].cpu().numpy(), plots) as fig:
             exp: SummaryWriter = self.logger.experiment # type: ignore
@@ -171,36 +172,14 @@ class Model(pl.LightningModule):
 # =============================================================================
 #
 #
-
-    def encode(
-        self, 
-        chart: Float[Tensor, str(f"B {X_DIM} L")],
-        audio: Float[Tensor, str(f"B {A_DIM} L")],
-    ) -> tuple[
-        Float[Tensor, "B Zx zL"],
-        Float[Tensor, "B Za zL"],
-    ]:
-        pad = self._padding(audio.size(-1))
-        if pad > 0:
-            chart = F.pad(chart, (0,pad))
-            audio = F.pad(audio, (0,pad), value=-1)
-        z_x = self.chart_encoder(chart)
-        z_a = self.audio_encoder(audio)
-        return z_x, z_a
     
     def generate(
         self,
         audio: Float[Tensor, str(f"{A_DIM} L")],
         make_latent: Callable[[Float[Tensor, "A zL"]], Float[Tensor, "B X zL"]],
     ) -> Float[Tensor, str(f"B {X_DIM} L")]:
-        pad = self._padding(audio.size(-1))
-        if pad > 0:
-            audio = F.pad(audio, (0,pad), value=-1)
         za = self.audio_encoder(audio[None])[0]
-        zx = make_latent(za)
-        za = repeat(za, 'a l -> b a l', b=zx.size(0))
-        q_zx, _ = self.vq(zx)
-        pred_chart = self.decoder(MP.cat([q_zx, za], dim=1))
-        if pad > 0:
-            pred_chart = pred_chart[...,:-pad]
+        q_zx, _ = self.vq(make_latent(za))
+        za = repeat(za, 'a l -> b a l', b=q_zx.size(0))
+        pred_chart = self.decoder(MP.cat([q_zx, za], dim=1))[:,:,:audio.size(-1)]
         return pred_chart.clamp(min=-1, max=1)
