31e9b4b1f3a52cd03698dc6c7d7aa4fcb20f8ff9
jason <jason@jwon.xyz>
2025-05-15 19:06:18 +0000

    audio_encoder: init

--- osu_dreamer/__main__.py
+++ osu_dreamer/__main__.py
@@ -11,6 +11,7 @@ import click
 from .scripts.predict import predict
 from .scripts.fit_denoiser import fit_denoiser
 from .scripts.fit_vqvae import fit_vqvae
+from .scripts.fit_audio_encoder import fit_audio_encoder
 from .scripts.generate_data import generate_data
 
 @click.group()
@@ -20,6 +21,7 @@ def main():
 main.add_command(generate_data)
 main.add_command(fit_denoiser)
 main.add_command(fit_vqvae)
+main.add_command(fit_audio_encoder)
 main.add_command(predict)
 
 if __name__ == "__main__":
