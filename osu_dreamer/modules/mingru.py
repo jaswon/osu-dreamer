8f01c3c0dc4bd86481b3f1e24c17d61f0ee85614
jason <jason@jwon.xyz>
2025-05-15 22:39:21 +0000

    mingru: refactor + recurrent

--- osu_dreamer/modules/mingru.py
+++ osu_dreamer/modules/mingru.py
@@ -1,5 +1,5 @@
 
-from jaxtyping import Float, Complex
+from jaxtyping import Float, Complex, Inexact
 
 import torch as th
 from torch import nn, Tensor
@@ -10,24 +10,50 @@ import osu_dreamer.modules.mp as MP
 def complex_log(float_input: Float[Tensor, "..."], eps=1e-6) -> Complex[Tensor, "..."]:
     real = th.clamp_min(float_input.abs(), eps).log()
     imag = (float_input < 0) * th.pi
-    return th.complex(real, imag.float())
+    return th.complex(real, imag.to(real.dtype))
+
+def log_parallel_scan(
+    log_a: Inexact[Tensor, "... L"],   # log(a[0:t])
+    log_b: Inexact[Tensor, "... L"],   # log(b[0:t])
+    log_h0: Inexact[Tensor, "..."],
+) -> Float[Tensor, "... L"]:
+    """
+    heinsen associative scan (log-space)
+    
+    returns h[0:t] such that:
+        - h[-1] = h0
+        - h[t] = a[t] * h[t-1] + b[t]
+    """
+    log_b = th.cat([log_h0[...,None], log_b], dim=-1)
+    a_star = F.pad(log_a.cumsum(dim=-1), (1,0))
+    log_h0_plus_b_star = th.logcumsumexp(log_b - a_star, dim=-1)
+    log_h = a_star + log_h0_plus_b_star
+    return log_h[...,1:].exp().real
 
 def min_gru(
     h: Float[Tensor, "... L"], 
     g: Float[Tensor, "... L"],
+    h0: None | Float[Tensor, "..."] = None,
 ) -> Float[Tensor, "... L"]:
-    log_coeffs = -F.softplus(g)
-    log_values = -F.softplus(-g) + complex_log(h)
+    """ o[t] = (1-σ(g[t])) * o[t-1] + σ(g[t]) * h[t] """
+    if h0 is None:
+        h0 = th.zeros_like(h[...,0])
+    return log_parallel_scan(
+        F.logsigmoid(-g),                   # 1-σ(g)
+        F.logsigmoid(g) + complex_log(h),   # σ(g) * h,
+        h0.log(),
+    ).to(h.dtype)
 
-    # heinsen associative scan (log-space)
-    a_star = log_coeffs.cumsum(dim=-1)
-    log_h0_plus_b_star = (log_values - a_star).logcumsumexp(dim=-1)
-    log_h = a_star + log_h0_plus_b_star
-    
-    return log_h.exp().real
+def min_gru_recurrent(
+    h: Float[Tensor, "..."],
+    g: Float[Tensor, "..."],
+    o: Float[Tensor, "..."],
+) -> Float[Tensor, "..."]:
+    """ o[t] = (1-σ(g[t])) * o[t-1] + σ(g[t]) * h[t] """
+    return th.lerp(o.to(h.dtype), h, F.sigmoid(g))
 
 class MinGRU(nn.Module):
-    def __init__(self, dim: int, out_dim: int = None):
+    def __init__(self, dim: int, out_dim: int | None = None):
         super().__init__()
         out_dim = out_dim or dim
         self.conv = MP.Conv1d(dim, dim, 5,1,2, groups=dim)
