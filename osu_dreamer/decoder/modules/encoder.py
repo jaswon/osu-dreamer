166a2b26bcd949270851497246f1cb168f34e578
jason <jason@jwon.xyz>
2025-05-23 19:29:24 +0000

    use mingru over deltanet for iteration

--- osu_dreamer/decoder/modules/encoder.py
+++ osu_dreamer/decoder/modules/encoder.py
@@ -8,8 +8,16 @@ from torch import nn, Tensor
 from torch.utils.checkpoint import checkpoint
 
 import osu_dreamer.modules.mp as MP
+from osu_dreamer.modules.mingru import MinGRU
 
-from .deltanet import DeltaNet, DeltaNetArgs
+
+class tokenMixer(nn.Module):
+    def __init__(self, dim: int, expand: int):
+        super().__init__()
+        self.mingru = MinGRU(dim, dim*expand)
+
+    def forward(self, x: Float[Tensor, "B L D"]) -> Float[Tensor, "B L D"]:
+        return self.mingru(x.transpose(1,2)).transpose(1,2)
     
 class channelMixer(nn.Module):
     def __init__(self, dim: int, expand: int):
@@ -39,10 +47,9 @@ class EncoderBlock(nn.Module):
         self,
         dim: int,
         expand: int,
-        deltanet_args: DeltaNetArgs,
     ):
         super().__init__()
-        self.seq_mixer = condBlock(dim, DeltaNet(dim, deltanet_args))
+        self.seq_mixer = condBlock(dim, tokenMixer(dim, expand))
         self.chn_mixer = condBlock(dim, channelMixer(dim, expand))
 
     def forward(self, x: Float[Tensor, "B N E"]) -> Float[Tensor, "B N E"]:
@@ -54,7 +61,6 @@ class EncoderBlock(nn.Module):
 class EncoderArgs:
     depth: int
     expand: int
-    deltanet_args: DeltaNetArgs
 
 class Encoder(nn.Module):
     def __init__(
@@ -64,7 +70,7 @@ class Encoder(nn.Module):
     ):
         super().__init__()
         self.blocks = nn.ModuleList([
-            EncoderBlock(dim, args.expand, args.deltanet_args)
+            EncoderBlock(dim, args.expand)
             for _ in range(args.depth)
         ])
 
