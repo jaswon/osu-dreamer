004e8615be32dedaa9050026664766fc386f03f2
jason <jason@jwon.xyz>
2025-06-10 17:50:47 +0000

    muon: clean up

--- osu_dreamer/modules/muon.py
+++ osu_dreamer/modules/muon.py
@@ -1,6 +1,5 @@
 
 import torch
-from typing import Generator
 
 # @torch.compile
 def zeropower_via_newtonschulz5(G, steps=10, eps=1e-7):
@@ -27,135 +26,71 @@ def zeropower_via_newtonschulz5(G, steps=10, eps=1e-7):
         X = X.T
     return X
 
-class Muon(torch.optim.Optimizer):
-    """
-    Muon - MomentUm Orthogonalized by Newton-schulz
+def muon_update(grad, momentum, beta=0.95, ns_steps=5, nesterov=True):
+    momentum.lerp_(grad, 1 - beta)
+    update = grad.lerp_(momentum, beta) if nesterov else momentum
+    orig_shape = update.shape
+    if update.ndim > 2:
+        update = update.view(len(update), -1)
+    update = zeropower_via_newtonschulz5(update, steps=ns_steps)
+    update *= max(1, update.size(0) / update.size(1))**0.5
+    return update.view(orig_shape)
 
-    - https://github.com/KellerJordan/Muon/blob/master/muon.py
-    - https://github.com/MoonshotAI/Moonlight/blob/master/examples/toy_train.py
-    """
+def adam_update(grad, buf1, buf2, step, betas, eps):
+    buf1.lerp_(grad, 1 - betas[0])
+    buf2.lerp_(grad.square(), 1 - betas[1])
+    buf1c = buf1 / (1 - betas[0]**step)
+    buf2c = buf2 / (1 - betas[1]**step)
+    return buf1c / (buf2c.sqrt() + eps)
+
+
+class Muon(torch.optim.Optimizer):
     def __init__(
         self, 
-        muon_params, 
-        lr=1e-3, 
+        param_groups,
+        lr=.02,
+        adam_lr=3e-4,
         weight_decay=0.1,
         momentum=0.95, 
-        nesterov=True, 
-        ns_steps=6,
-        adamw_params=None, 
-        adamw_betas=(0.95, 0.95), 
-        adamw_eps=1e-8, 
+        adamw_betas=(0.9, 0.95), 
+        adamw_eps=1e-10,
     ):
-
-        defaults = dict(
-            lr=lr, 
-            momentum=momentum, 
-            nesterov=nesterov, 
+        super().__init__(param_groups, dict(
+            lr=lr,
+            adam_lr=adam_lr,
+            momentum=momentum,
             weight_decay=weight_decay,
-            ns_steps=ns_steps,
             adamw_betas=adamw_betas, 
-            adamw_eps=adamw_eps, 
-        )
+            adamw_eps=adamw_eps,
+        ))
 
-        # handle list of params or list of dicts
-        if isinstance(muon_params, Generator):
-            muon_params = list(muon_params)
-        if isinstance(adamw_params, Generator):
-            adamw_params = list(adamw_params)
-        elif adamw_params is None:
-            adamw_params = []
-
-        super().__init__([*muon_params, *adamw_params], defaults)
-
-        # Sort parameters into those for which we will use Muon, and those for which we will not
-        # we cant pickle booleans for saving, so we will use 1=True, 0=False
-        def assign_muon(p):
-            if p.ndim >= 2 and p.size(0) < 10000:
-                self.state[p]['use_muon'] = 1
-            else:
-                self.state[p]['use_muon'] = 0
-
-        if isinstance(muon_params[0], dict):
-            for group in muon_params:
-                for p in group['params']:
-                    assign_muon(p)
-        else:
-            for p in muon_params:
-                assign_muon(p)
-
-        def assign_adamw(p):
-            # Do not use Muon for parameters in adamw_params
-            self.state[p]['use_muon'] = 0
-
-        if len(adamw_params) and isinstance(adamw_params[0], dict):
-            for group in adamw_params:
-                for p in group['params']:
-                    assign_adamw(p)
-        else:
-            for p in adamw_params:
-                assign_adamw(p)
-
-        if torch.distributed.is_initialized():
-            self.world_size = torch.distributed.get_world_size()
-            self.rank = torch.distributed.get_rank()
-        else:
-            self.world_size = 1
-            self.rank = 0
-
-    def step(self, closure=None):
-        """Perform a single optimization step.
-
-        Args:
-            closure (Callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-
+    @torch.no_grad
+    def step(self):
         for group in self.param_groups:
-            lr = group["lr"]
-            momentum = group['momentum']
-            weight_decay = group['weight_decay']
             for p in group['params']:
                 state = self.state[p]
-                g = p.grad
-                if g is None:
+                if p.grad is None:
                     continue
 
-                if state['use_muon'] == 1:
-                    if g.ndim > 2:
-                        g = g.view(g.size(0), -1)
-
-                    if 'momentum_buffer' not in state:
-                        state['momentum_buffer'] = torch.zeros_like(g)
-                    buf = state['momentum_buffer']
-                    buf.mul_(momentum).add_(g)
-                    if group['nesterov']:
-                        g = g.add(buf, alpha=momentum)
-
-                    # gives NaNs when done with Dtensor, instead of throwing a typical op not supported error, quite sneaky
-                    g = zeropower_via_newtonschulz5(g, steps=group['ns_steps'])
-                    p.data.mul_(1 - lr * weight_decay)
-                    p.data.add_(
-                        g.view_as(p.data).type_as(p.data), 
-                        alpha = -lr * .2 * max(g.size(0),g.size(1))**0.5,
-                    )
-                else:
+                if p.ndim < 2:
+                    # adamw update
+                    lr = group["adam_lr"]
                     if 'step' not in state:
                         state['step'] = 0
-                        state['moment1'] = torch.zeros_like(g)
-                        state['moment2'] = torch.zeros_like(g)
+                        state['moment1'] = torch.zeros_like(p.grad)
+                        state['moment2'] = torch.zeros_like(p.grad)
                     state['step'] += 1
-                    step = state['step']
-                    buf1 = state['moment1']
-                    buf2 = state['moment2']
-                    buf1.lerp_(g, 1-group['adamw_betas'][0])
-                    buf2.lerp_(g.square(), 1-group['adamw_betas'][1])
+                    update = adam_update(
+                        p.grad, 
+                        state['moment1'], state['moment2'], state['step'], 
+                        group['adamw_betas'], group['adamw_eps'],
+                    )
+                else:
+                    # muon update
+                    lr = group["lr"]
+                    if 'momentum_buffer' not in state:
+                        state['momentum_buffer'] = torch.zeros_like(p.grad)
+                    update = muon_update(p.grad, state["momentum_buffer"], group["momentum"])
 
-                    g = buf1 / (group['adamw_eps'] + buf2.sqrt())
-
-                    bias_correction1 = 1 - group['adamw_betas'][0]**step
-                    bias_correction2 = 1 - group['adamw_betas'][1]**step
-                    scale = bias_correction1 / bias_correction2**0.5
-                    p.data.mul_(1 - lr * weight_decay)
-                    p.data.add_(g, alpha=-lr/scale)
-
-
+                p.mul_(1 - lr * group['weight_decay'])
+                p.add_(update, alpha=-lr)
