4c35629f487d207826a930bda2f95262e4a2b8f1
jason <jason@jwon.xyz>
2025-06-15 17:58:19 +0000

    vqvae: expanded wavenet

--- osu_dreamer/vqvae/modules/wavenet.py
+++ osu_dreamer/vqvae/modules/wavenet.py
@@ -7,27 +7,31 @@ from torch import nn, Tensor
 
 import osu_dreamer.modules.mp as MP
 
-from .se import SqueezeExcitation
-
 X = Float[Tensor, "B X L"]
 
 class WaveNetLayer(nn.Module):
-    def __init__(self, dim: int, squeeze: int, d: int):
+    def __init__(self, dim: int, d: int, expand: int):
         super().__init__()
-        self.filter = MP.Conv1d(dim, dim, 3, dilation=2**d, padding=2**d, groups=dim)
-        self.gate = MP.Conv1d(dim, dim, 3, dilation=2**d, padding=2**d, groups=dim)
-        self.res = MP.Conv1d(dim, dim, 1)
-        self.se = SqueezeExcitation(dim, squeeze)
+        h_dim = dim * expand
+        self.filter = nn.Sequential(
+            MP.Conv1d(dim, h_dim, 1),
+            MP.Conv1d(h_dim, h_dim, 3, dilation=d, padding=d, groups=h_dim),
+        )
+        self.gate = nn.Sequential(
+            MP.Conv1d(dim, h_dim, 1),
+            MP.Conv1d(h_dim, h_dim, 3, dilation=d, padding=d, groups=h_dim),
+        )
+        self.res = MP.Conv1d(h_dim, dim, 1)
 
     def forward(self, x: X) -> X:
-        res = self.res(self.filter(x).tanh() / .627 * MP.sigmoid(self.gate(x)))
-        return MP.add(x, self.se(res))
+        res = self.res(self.filter(x) * MP.sigmoid(self.gate(x)))
+        return MP.add(x, res, t=.3)
     
 @dataclass
 class WaveNetArgs:
     num_stacks: int
     stack_depth: int
-    squeeze: int
+    expand: int
 
 class WaveNet(nn.Sequential):
     def __init__(
@@ -36,7 +40,7 @@ class WaveNet(nn.Sequential):
         args: WaveNetArgs,
     ):
         super().__init__(*[
-            WaveNetLayer(dim, args.squeeze, d)
+            WaveNetLayer(dim, 2**d, args.expand)
             for _ in range(args.num_stacks)
             for d in range(args.stack_depth)
         ])
