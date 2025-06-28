a46cf0eb187124b0112eae793b177dd88982953b
jason <jason@jwon.xyz>
2025-06-28 00:58:23 +0000

    latent: revert to gaussian bottleneck

--- osu_dreamer/latent_model/model.py
+++ osu_dreamer/latent_model/model.py
@@ -20,7 +20,7 @@ import osu_dreamer.modules.mp as MP
 
 from .data.module import Batch
 
-from .modules.power_spherical import PowerSphericalVariationalBottleneck
+from .modules.gaussian import GaussianVariationalBottleneck
 from .modules.ae import Encoder, Decoder, AutoEncoderArgs
 from .modules.critic import MultiScaleCritic, MultiScaleCriticArgs
 
@@ -78,7 +78,7 @@ class Model(pl.LightningModule):
         self.encoder = nn.Sequential(
             MP.Conv1d(X_DIM, h_dim, 1),
             Encoder(h_dim, ae_args),
-            PowerSphericalVariationalBottleneck(h_dim, emb_dim),
+            GaussianVariationalBottleneck(h_dim, emb_dim),
         )
         self.decoder = nn.Sequential(
             MP.PixelNorm(),
