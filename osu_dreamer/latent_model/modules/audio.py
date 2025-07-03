4fea9f4712f3e6ef6fca3839c4b6e90412e53e4e
jason <jason@jwon.xyz>
2025-07-03 18:09:51 +0000

    latent: film audio encoder

--- osu_dreamer/latent_model/modules/audio.py
+++ osu_dreamer/latent_model/modules/audio.py
@@ -1,19 +1,32 @@
 
 from jaxtyping import Float
 
-import torch as th
 from torch import nn, Tensor
-
 
 import osu_dreamer.modules.mp as MP
 from osu_dreamer.modules.spec_features import SpecFeatures
+
+class FiLM(nn.Module):
+    def __init__(self, dim: int, h_dim: int):
+        super().__init__()
+        self.gamma = nn.Sequential(MP.Conv1d(h_dim, dim, 1), MP.Gain())
+        self.beta = nn.Sequential(MP.Conv1d(h_dim, dim, 1), MP.Gain())
+
+    def forward(
+        self,
+        x: Float[Tensor, "B X L"],
+        m: Float[Tensor, "B H L"],
+    ) -> Float[Tensor, "B X L"]:
+        return self.beta(m) + (1+self.gamma(m)) * x
 
 class AudioEncoder(nn.Module):
     def __init__(self, out_dim: int, in_dim: int, h_dim: int):
         super().__init__()
         self.encoder = SpecFeatures(h_dim)
+        self.film = FiLM(in_dim, h_dim)
         self.decoder = nn.Sequential(
-            MP.Conv1d(h_dim+in_dim, out_dim, 1),
+            MP.Conv1d(in_dim, in_dim, 5,1,2, groups=in_dim),
+            MP.Conv1d(in_dim, out_dim, 1),
             MP.Gain(),
         )
 
@@ -24,4 +37,5 @@ class AudioEncoder(nn.Module):
     ) -> Float[Tensor, "B X L"]:
         x = x[:,:,:a.size(-1)]
         h = self.encoder(a).expand(x.size(0),-1,-1)
-        return self.decoder(th.cat([x,h], dim=1))
+        x = self.film(x, h)
+        return self.decoder(x)
