5c442d35fa7c15f6e65f16577558f09e735286b5
Jason Won <jason@jwon.xyz>
2026-07-01 10:41:22 -0400

    modules: rms norm affine

--- osu_dreamer/modules/rms_norm.py
+++ osu_dreamer/modules/rms_norm.py
@@ -5,11 +5,16 @@ import torch as th
 from torch import nn, Tensor
 
 class RMSNorm1d(nn.Module):
-    def __init__(self, dim: int):
+    def __init__(self, dim: int, affine: bool = True):
         super().__init__()
-        self.gamma = nn.Parameter(th.randn(dim, 1))
+        self.affine = affine
+        if affine:
+            self.gamma = nn.Parameter(th.randn(dim))
 
     def forward(self, x: Float[Tensor, "B D L"]) -> Float[Tensor, "B D L"]:
         xf = x.float()
         inv_rms = xf.pow(2).mean(dim=1, keepdim=True).add(1e-6).rsqrt()
-        return (xf * inv_rms).to(x.dtype) * self.gamma
+        normed = (xf * inv_rms).to(x.dtype)
+        if self.affine:
+            normed = normed * self.gamma[:,*((None,) * (x.ndim-2))]
+        return normed
