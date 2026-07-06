8e9e9c14f974df1c160d8b0e39c4dc82901ef229
Jason Won <jason@jwon.xyz>
2026-07-06 10:06:04 -0400

    latent: chunk unmixer + cat mixer

--- osu_dreamer/models/latent/unet.py
+++ osu_dreamer/models/latent/unet.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from jaxtyping import Float
 
+import torch as th
 from torch import nn, Tensor
 
 from osu_dreamer.modules.res import Res
@@ -28,14 +29,15 @@ class UNetEncoder(nn.Module):
     ):
         super().__init__()
         self.down = nn.AvgPool1d(stride)
-
         self.layers = nn.ModuleList([ Layer(dim, args.n_layers, args.radius) for _ in range(n_downs) ])
+        self.unmixers = nn.ModuleList([ nn.Conv1d(dim, 2*dim, 1) for _ in range(n_downs) ])
 
     def forward(self, x: Float[Tensor, "B X L"]) -> tuple[ list[ Float[Tensor, "B X _l"] ], Float[Tensor, "B X l"] ]:
         skips = []
-        for layer in self.layers:
-            h = layer(x)
-            skips.append(h)
+        for layer, unmixer in zip(self.layers, self.unmixers):
+            x = layer(x)
+            skip, x = unmixer(x).chunk(2, dim=1)
+            skips.append(skip)
             x = self.down(x)
         return skips, x
     
@@ -49,28 +51,13 @@ class UNetDecoder(nn.Module):
     ):
         super().__init__()
         self.up = nn.Upsample(scale_factor=stride)
-
         self.layers = nn.ModuleList([ Layer(dim, args.n_layers, args.radius) for _ in range(n_downs) ])
-        self.mixers = nn.ModuleList([ AdaLN1d(dim, dim) for _ in range(n_downs) ])
+        self.mixers = nn.ModuleList([ nn.Conv1d(2*dim, dim, 1) for _ in range(n_downs) ])
 
     def forward(self, skips: list[ Float[Tensor, "#B X _L"] ], x: Float[Tensor, "B X l"]) -> Float[Tensor, "B X L"]:
         for mix, layer in zip(self.mixers, self.layers):
             x = self.up(x)
-            x = mix(skips.pop().expand(x.size(0), -1, -1), x)
+            skip = skips.pop().expand(x.size(0), -1, -1)
+            x = mix(th.cat([x, skip], dim=1))
             x = layer(x)
         return x
-
-    
-class AdaLN1d(nn.Module):
-    def __init__(self, dim: int, cond_dim: int):
-        super().__init__()
-        self.norm = RMSNorm(dim, affine=False)
-        self.proj = nn.Conv1d(cond_dim, dim*2, 1)
-
-    def forward(
-        self,
-        x: Float[Tensor, "B X L"],
-        c: Float[Tensor, "B C L"],
-    ) -> Float[Tensor, "B X L"]:
-        scale, shift = self.proj(c).chunk(2, dim=1)
-        return self.norm(x) * (1 + scale) + shift
