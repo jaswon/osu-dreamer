cf68dfc13606837b7835ec7ec6a3c6c82e2281ee
Jason Won <jason@jwon.xyz>
2026-07-08 20:32:38 -0400

    latent: no label predictor grad factor

--- osu_dreamer/models/latent/label_predictor.py
+++ osu_dreamer/models/latent/label_predictor.py
@@ -2,18 +2,15 @@
 from dataclasses import dataclass
 from jaxtyping import Float
 
-import torch as th
 from torch import nn, Tensor
 
 @dataclass
 class LabelPredictorArgs:
     h_dim: int
-    grad_factor: float
 
 class LabelPredictor(nn.Module):
     def __init__(self, x_dim: int, label_dim: int, args: LabelPredictorArgs):
         super().__init__()
-        self.grad_factor = args.grad_factor
         self.net = nn.Sequential(
             nn.Linear(x_dim, args.h_dim),
             nn.SiLU(),
@@ -21,6 +18,4 @@ class LabelPredictor(nn.Module):
         )
 
     def forward(self, x: Float[Tensor, "B X"]) -> Float[Tensor, "B C"]:
-        if self.training:
-            x = th.lerp(x.detach(), x, self.grad_factor)
         return 5 + 5 * self.net(x)
