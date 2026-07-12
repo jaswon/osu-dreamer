35661a614d2cb8a23af55bc702dbb63ca9f82e5f
Jason Won <jason@jwon.xyz>
2026-07-12 19:58:08 -0400

    rff: frozen weights and biases

--- osu_dreamer/common/fourier_features.py
+++ osu_dreamer/common/fourier_features.py
@@ -6,12 +6,12 @@ from torch import nn, Tensor
 
 
 class FourierFeatures(nn.Module):
-    def __init__(self, dim: int, features: int, freq_scale: float = 16.):
+    def __init__(self, dim: int, features: int, n_bins: int = 16):
         super().__init__()
-        self.proj_in = nn.Linear(dim, features)
-        th.nn.init.normal_(self.proj_in.weight, std=freq_scale)
-        th.nn.init.uniform_(self.proj_in.bias, -th.pi, th.pi)
+        freq_scale = float(n_bins)
+        self.W: th.Tensor; self.register_buffer('W', th.randn(features, dim) * freq_scale)
+        self.b: th.Tensor; self.register_buffer('b', th.empty(features).uniform_(-th.pi, th.pi))
         self.scale = (2/features) ** .5
 
     def forward(self, x: Float[Tensor, "*B I"]) -> Float[Tensor, "*B O"]:
-        return self.scale * th.cos(self.proj_in(x))
+        return self.scale * th.cos(x @ self.W.T + self.b)
